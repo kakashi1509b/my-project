2e4c36a6ed7f0df1a9ddc407a0c10554747d28ff
kakashi1509b <bharat.5224varfx@gmail.com>
2024-11-07 21:20:00 +0530

    Update checkifpalindrome.py

--- checkifpalindrome.py
+++ checkifpalindrome.py
@@ -1,10 +1,11 @@
 print("Check if your keyword is palindrome")
-def checkifpalindrome(a):
-    a=str(input("Enter your keyword: "))
-    lst1=[i for i in a]   
+a=str(input("Enter your keyword: "))
+def checkifpalindrome(n):    
+    lst1=[i for i in n]   
     lst2=lst1.copy()
     lst2.reverse()
     if lst1==lst2:
         print("It is palindrome")
     else:
         print("It is not palindrome")    
+checkifpalindrome(a)
